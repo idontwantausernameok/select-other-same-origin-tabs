1cad6784d54e187dd85757f6c60920e2f91bffaf
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-06-10 14:52:57 +0200

    first

---
+++ background.js
@@ -0,0 +1,36 @@
+//const temporary = browser.runtime.id.endsWith('@temporary-addon'); // debugging?
+const manifest = browser.runtime.getManifest();
+const extname = manifest.name;
+const extdesc = manifest.description
+
+browser.menus.create({
+	id: extname,
+	title: extdesc,
+	contexts: ["tab"],
+	onclick: async function(info, tab) {
+		if(info.menuItemId.startsWith(extname)){
+			const url = new URL(tab.url);
+			const tabs = await browser.tabs.query({ url: url.origin + "/*", hidden: false});
+
+			let tmp = {};
+
+			//tmp[tab.windowId] = [tab.index];
+
+			tabs.forEach( (t) => {
+				if (typeof tmp[t.windowId] === 'undefined'){
+					tmp[t.windowId] = [];
+				} 
+				tmp[t.windowId].push(t.index);
+			});
+
+			for (const [k,v] of Object.entries(tmp)) {
+				browser.tabs.highlight({
+					windowId: parseInt(k),
+					tabs: v,
+					populate: false
+				}); 
+			}
+		}
+	}
+});
+
